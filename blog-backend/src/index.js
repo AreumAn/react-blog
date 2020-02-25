71262537fc9d4ed60c8390cce52359b9bb53661d
AREUM AN <areum.an.0928@gmail.com>
2020-02-25 04:32:24 -0500

    Install nodemon

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -2,6 +2,20 @@ const Koa = require('koa');
 
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+    console.log(ctx.url);
+    console.log(1);
+
+    await next();
+    console.log('END');
+})
+
+app.use((ctx, next) => {
+    console.log(2);
+    next();
+})
+
+// ctx: context - it has informations about request and response
 app.use(ctx => {
     ctx.body = 'hello world';
 });
