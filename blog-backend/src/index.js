dd94dda58d1314a4fc910ae794796de4c60943f4
AREUM AN <areum.an.0928@gmail.com>
2020-02-25 04:09:29 -0500

    Set port 4000 server

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,1 +1,11 @@
-const hello = 'hello';
+const Koa = require('koa');
+
+const app = new Koa();
+
+app.use(ctx => {
+    ctx.body = 'hello world';
+});
+
+app.listen(4000, () => {
+    console.log('Listening to port 4000');
+});
