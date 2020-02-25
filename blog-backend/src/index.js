c5385d256f8514172722ec20a192666345d87a9c
AREUM AN <areum.an.0928@gmail.com>
2020-02-25 05:02:01 -0500

    Add router to test

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,11 +1,19 @@
 const Koa = require('koa');
+const Router = require('koa-router');
 
 const app = new Koa();
+const router = new Router();
 
-// ctx: context - it has informations about request and response
-app.use(ctx => {
-    ctx.body = 'hello world';
+// Set router
+router.get('/', ctx => {
+    ctx.body = 'Home';
 });
+
+router.get('/about', ctx => {
+    ctx.body = 'About';
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
     console.log('Listening to port 4000');
