76c2f2836a9908e5db80dc5f9e5f022bd2d26afd
AREUM AN <areum.an.0928@gmail.com>
2020-02-26 15:58:13 -0500

    Add data  post, get, delete, patch with DB

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,107 +1,94 @@
-let postId = 1;
-
-// initial post data
-const posts = [
-    {
-        id: 1,
-        title: 'This is title',
-        body: 'Content will be here!',
-    },
-];
+import Post from '../../models/post';
 
 /*  
     New post
-    POST /api/posts - { title, body }
+    POST /api/posts
+    { 
+        title: string,
+        body: string,
+        tags: [string]
+    }
 */
-export const write = ctx => {
-    const { title, body } = ctx.request.body;
-    postId += 1;
-    const post = { id: postId, title, body };
-    posts.push(post);
-    ctx.body = post;
+export const write = async ctx => {
+    const { title, body, tags } = ctx.request.body;
+    const post = new Post({
+        title,
+        body,
+        tags,
+    });
+    try {
+        await post.save();
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
 };
 
 /* 
     Get list of posts data
     GET /api/posts
 */
-export const list = ctx => {
-    ctx.body = posts;
+export const list = async ctx => {
+    try {
+        const posts = await Post.find().exec();
+        ctx.body = posts;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
 };
 
 /* 
     Get post data by id
     GET /api/posts/:id
 */
-export const read = ctx => {
+export const read = async ctx => {
     const { id } = ctx.params;
-    const post = posts.find(p => p.id.toString() === id);
-    if (!post) {
-        ctx.status = 404;
-        ctx.body = {
-            message: 'No post',
-        };
-        return;
+    try {
+        const post = await Post.findById(id).exec();
+        if (!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
     }
-    ctx.body = post;
 };
 
 /*
     DELETE /api/posts/:id
 */
-export const remove = ctx => {
+export const remove = async ctx => {
     const { id } = ctx.params;
-    const index = posts.findIndex(p => p.id.toString() === id);
-    if (index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: 'No post',
-        };
-        return;
+    try {
+        await Post.findByIdAndRemove(id).exec();
+        ctx.status = 204; // No content
+    } catch (e) {
+        ctx.throw(500, e);
     }
-    // Remove post by index
-    posts.splice(index, 1);
-    ctx.status = 204; // No Content
-};
-
-/* 
-    Modify post
-    PUT /api/posts/:id - { title, body }
-*/
-export const replace = ctx => {
-    const { id } = ctx.params;
-    const index = posts.findIndex(p => p.id.toString() === id);
-    if (index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: 'No post',
-        };
-        return;
-    }
-    posts[index] = {
-        id,
-        ...ctx.request.body,
-    };
-    ctx.body = posts[index];
 };
 
 /* 
     Update post filed
-    PATCH /api/posts/:id - { title, body }
-*/
-export const update = ctx => {
-    const { id } = ctx.params;
-    const index = posts.findIndex(p => p.id.toString() === id);
-    if (index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: 'No post',
-        };
-        return;
+    PATCH /api/posts/:id
+    { 
+        title: string,
+        body: string,
+        tags: [string]
     }
-    posts[index] = {
-        ...posts[index],
-        ...ctx.request.body,
-    };
-    ctx.body = posts[index];
+*/
+export const update = async ctx => {
+    const { id } = ctx.params;
+    try {
+        const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+            new: true,
+        }).exec();
+        if (!post) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.body = post;
+    } catch (e) {
+        ctx.throw(500, e);
+    }
 };
