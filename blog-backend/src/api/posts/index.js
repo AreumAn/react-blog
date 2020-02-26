fd07dbc8978dd541de7d1356d13bf021cbbce409
AREUM AN <areum.an.0928@gmail.com>
2020-02-26 16:06:35 -0500

    Add validation of id in url

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -2,11 +2,14 @@ import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
-
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.patch('/:id', postsCtrl.update);
+
+const post = new Router(); // /api/posts/:id
+post.get('/', postsCtrl.read);
+post.delete('/', postsCtrl.remove);
+post.patch('/', postsCtrl.update);
+
+posts.use('/:id', postsCtrl.checkObjectId, post.routes());
 
 export default posts;
