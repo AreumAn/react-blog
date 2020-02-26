8470d069376079b8c825f83cecff31a499c70ea9
AREUM AN <areum.an.0928@gmail.com>
2020-02-26 03:05:28 -0500

    Add API test without DB

---
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -0,0 +1,107 @@
+let postId = 1;
+
+// initial post data
+const posts = [
+    {
+        id: 1,
+        title: 'This is title',
+        body: 'Content will be here!',
+    },
+];
+
+/*  
+    New post
+    POST /api/posts - { title, body }
+*/
+exports.write = ctx => {
+    const { title, body } = ctx.request.body;
+    postId += 1;
+    const post = { id: postId, title, body };
+    posts.push(post);
+    ctx.body = post;
+};
+
+/* 
+    Get list of posts data
+    GET /api/posts
+*/
+exports.list = ctx => {
+    ctx.body = posts;
+};
+
+/* 
+    Get post data by id
+    GET /api/posts/:id
+*/
+exports.read = ctx => {
+    const { id } = ctx.params;
+    const post = posts.find(p => p.id.toString() === id);
+    if (!post) {
+        ctx.status = 404;
+        ctx.body = {
+            message: 'No post',
+        };
+        return;
+    }
+    ctx.body = post;
+};
+
+/*
+    DELETE /api/posts/:id
+*/
+exports.remove = ctx => {
+    const { id } = ctx.params;
+    const index = posts.findIndex(p => p.id.toString() === id);
+    if (index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: 'No post',
+        };
+        return;
+    }
+    // Remove post by index
+    posts.splice(index, 1);
+    ctx.status = 204; // No Content
+};
+
+/* 
+    Modify post
+    PUT /api/posts/:id - { title, body }
+*/
+exports.replace = ctx => {
+    const { id } = ctx.params;
+    const index = posts.findIndex(p => p.id.toString() === id);
+    if (index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: 'No post',
+        };
+        return;
+    }
+    posts[index] = {
+        id,
+        ...ctx.request.body,
+    };
+    ctx.body = posts[index];
+};
+
+/* 
+    Update post filed
+    PATCH /api/posts/:id - { title, body }
+*/
+exports.update = ctx => {
+    const { id } = ctx.params;
+    const index = posts.findIndex(p => p.id.toString() === id);
+    if (index === -1) {
+        ctx.status = 404;
+        ctx.body = {
+            message: 'No post',
+        };
+        return;
+    }
+    posts[index] = {
+        ...posts[index],
+        ...ctx.request.body,
+    };
+    ctx.body = posts[index];
+};
--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,12 +1,19 @@
 const Koa = require('koa');
 const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
 const api = require('./api');
 
 const app = new Koa();
 const router = new Router();
 
+// Set router
 router.use('/api', api.routes());
 
+// Apply bodyParser before router
+app.use(bodyParser());
+
+// Apply router to app instance
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
