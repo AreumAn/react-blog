5ec9f5e470a9be69b464f8e6b1afece93514f15d
AREUM AN <areum.an.0928@gmail.com>
2020-03-01 04:44:39 -0500

    Add Route

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import { Route } from 'react-router-dom';
+import PostListpage from './pages/PostListPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
 
 function App() {
-    return <div>Frontend</div>;
+    return (
+        <>
+            <Route component={PostListpage} path={['/@:username', '/']} exact />
+            <Route component={LoginPage} path={'/login'} />
+            <Route component={RegisterPage} path={'/register'} />
+            <Route component={WritePage} path={'/write'} />
+            <Route component={PostPage} path={'/@:username/:postId'} />
+            
+        </>
+    );
 }
 
 export default App;
---
+++ blog-frontend/src/pages/PostListPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostListpage = () => {
+    return <div>PostListpage</div>
+}
+
+export default PostListpage;
