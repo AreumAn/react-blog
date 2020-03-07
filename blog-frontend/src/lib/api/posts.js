11ee29a004f5065672b6bf80a39c54aae5d549f0
AREUM AN <areum.an.0928@gmail.com>
2020-03-07 08:47:29 -0500

    Add post detail page

--- blog-frontend/src/lib/api/posts.js
+++ blog-frontend/src/lib/api/posts.js
@@ -1,3 +1,4 @@
 import client from './client';
 
 export const writePost = ({ title, body, tags }) => client.post('/api/posts', { title, body, tags });
+export const readPost = id => client.get(`/api/posts/${id}`);
