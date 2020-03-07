b9c5103045097a85f079c11ba6503f88115831a5
AREUM AN <areum.an.0928@gmail.com>
2020-03-06 23:35:05 -0500

    Add TagBoxContainer using redux

--- blog-frontend/src/components/write/TagBox.js
+++ blog-frontend/src/components/write/TagBox.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import styled from 'styled-components';
 import palette from '../../lib/styles/palette';
 
@@ -72,7 +72,7 @@ const TagList = React.memo(({ tags, onRemove }) => (
     </TagListBlock>
 ));
 
-const TagBox = () => {
+const TagBox = ({ tags, onChangeTags }) => {
     const [input, setInput] = useState('');
     const [localTags, setLocalTags] = useState([]);
     
@@ -80,16 +80,20 @@ const TagBox = () => {
         tag => {
             if(!tag) return;
             if(localTags.includes(tag)) return;
-            setLocalTags([...localTags, tag]);
+            const nextTags = [...localTags, tag];
+            setLocalTags(nextTags);
+            onChangeTags(nextTags);
         },
-        [localTags],
+        [localTags, onChangeTags],
     );
 
     const onRemove = useCallback(
         tag => {
-            setLocalTags(localTags.filter(t => t !== tag));
+            const nextTags = localTags.filter(t => t !== tag);
+            setLocalTags(nextTags);
+            onChangeTags(nextTags);
         },
-        [localTags],
+        [localTags, onChangeTags],
     );
 
     const onChange = useCallback(
@@ -107,6 +111,10 @@ const TagBox = () => {
         },
         [input, insertTag],
     );
+
+    useEffect(() => {
+        setLocalTags(tags);
+    }, [tags]);
 
     return(
         <TagBoxBlock>
--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Responsive from "../components/common/Responsive";
-import TagBox from "../components/write/TagBox";
 import WriteActionButtons from "../components/write/WriteActionButtons";
 import EditorContainer from "../containers/write/EditorContainer";
+import TagBoxContainer from "../containers/write/TagBoxContainer";
 
 const WritePage = () => {
     return (
         <Responsive>
             <EditorContainer />
-            <TagBox />
+            <TagBoxContainer />
             <WriteActionButtons />
         </Responsive>
     );
