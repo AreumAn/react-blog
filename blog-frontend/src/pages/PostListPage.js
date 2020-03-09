0174324674d0e4ef74eb777165e30b2b22accc08
AREUM AN <areum.an.0928@gmail.com>
2020-03-08 22:48:39 -0400

    Add pagination

--- blog-frontend/src/pages/PostListPage.js
+++ blog-frontend/src/pages/PostListPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import HeaderContainer from "../containers/common/HeaderContainer";
 import PostListContainer from "../containers/posts/PostListContainer";
+import PaginationContainer from "../containers/posts/PaginationContainer";
 
 const PostListpage = () => {
     return (
         <>
             <HeaderContainer />
             <PostListContainer />
+            <PaginationContainer />
         </>
     );
 }
